0814fc62660b74ee66ef0327838843efaad8bcee
nurmf <s952122@0yy6d.onmicrosoft.com>
2023-04-01 03:00:21 +0800

    add swagger-ui

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,13 +15,13 @@ public class TestController {
     @Autowired
     RedisTemplate redisTemplate;
 
-    @RequestMapping("/redis/add")
+    @GetMapping("/redis/add")
     public String addRedis() {
         redisTemplate.opsForValue().set("liongogoRedisTest", "I am Lion GoGo");
         return "加入成功";
     }
 
-    @RequestMapping("/redis/get")
+    @GetMapping("/redis/get")
     public Object getRedis() {
         Object value = redisTemplate.opsForValue().get("liongogoRedisTest");
         return value;
