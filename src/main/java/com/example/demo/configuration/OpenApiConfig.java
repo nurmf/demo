7669e1b5b886d39848c8c0a75148fa0f13f70f46
nurmf <s952122@0yy6d.onmicrosoft.com>
2023-04-01 03:21:59 +0800

    rename config package

--- src/main/java/com/example/demo/configuration/OpenApiConfig.java
+++ src/main/java/com/example/demo/configuration/OpenApiConfig.java
@@ -1,4 +1,4 @@
-package com.example.demo.Configuration;
+package com.example.demo.configuration;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
